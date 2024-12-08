4aedc5bf72f108a52be062a86bdbed1f991c3f05
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-12-08 12:24:00 +0900

    add:speed_checker

---
+++ speed_checker.py
@@ -0,0 +1,17 @@
+import time
+import requests
+from datetime import datetime
+
+url = "https://showdeerocks.com/data"
+
+start_time = time.time()
+response = requests.get(url)
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+measurement_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+print(f"응답까지 걸린 시간: {elapsed_time:.2f}초")
+
+with open("response_time.txt", "a") as f:
+    f.write(f"{measurement_time}:{elapsed_time}\n")
