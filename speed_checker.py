3219e5b3e6221f58e7b89b6d1e6b348ff45dea6f
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-12-08 12:26:34 +0900

    fix: format of the outputs

--- speed_checker.py
+++ speed_checker.py
@@ -14,4 +14,4 @@ measurement_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print(f"응답까지 걸린 시간: {elapsed_time:.2f}초")
 
 with open("response_time.txt", "a") as f:
-    f.write(f"{measurement_time}:{elapsed_time}\n")
+    f.write(f"{measurement_time}에 측정한 서버 반응속도 : {elapsed_time}\n")
